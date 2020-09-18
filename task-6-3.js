b4bd6462341b2d3a37b9829a8fbbf8ac6a9f7330
Smekalin Sergei <crypto.mnr@gmail.com>
2020-09-18 17:44:31 +0300

    Сделал 2 задачу

---
+++ task-6-3.js
@@ -0,0 +1,19 @@
+/*Задача 6-3
+filter, map
+Получи массив имен пользователей (значение свойства name) по полу (значение свойства gender).
+
+Используй деструктурирующее присваивание для параметра функции ({name}) без пробелов и переносов на новую строку.
+
+Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.*/
+
+// Write code under this line
+const getUsersWithGender = (array, gender) => array.map(({ name }) => name).filter(user.gender == gender);
+
+// console.log(getUsersWithGender(users, 'male'));
+
+/* [
+  "Moore Hensley",
+  "Ross Vazquez",  
+  "Carey Barr",  
+  "Blackburn Dotson"  
+] */
